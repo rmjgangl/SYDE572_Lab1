f728154bbfed8cffb60c6127bbd9ef55ad694538
Ryan Gangl <39352084+rmjgangl@users.noreply.github.com>
2021-02-19 14:39:36 -0500

    Create plot_ellipse.m

---
+++ plot_ellipse.m
@@ -0,0 +1,8 @@
+function plot_ellipse(x,y,theta,a,b)
+
+if nargin<5, error('Too few arguments to Plot_Ellipse.'); end;
+
+np = 100;
+ang = [0:np]*2*pi/np;
+pts = [x;y]*ones(size(ang)) + [cos(theta) -sin(theta); sin(theta) cos(theta)]*[cos(ang)*a; sin(ang)*b];
+plot( pts(1,:), pts(2,:) );

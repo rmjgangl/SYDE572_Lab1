a241dd9eb3411a91e8f834c740b3f278b5977fa2
Ryan Gangl <39352084+rmjgangl@users.noreply.github.com>
2021-02-24 14:20:52 -0500

    Update kNN.m

--- kNN.m
+++ kNN.m
@@ -13,14 +13,15 @@ function kNN(X1,X2)
     for i = 1:length(XGrid)
         for j = 1:length(X1)
             Class1min(j) = sqrt((X1(j,1) - XGrid(i,1))^2 + (X1(j,2) - XGrid(i,2))^2);
-            [vals1] = sort(Class1min);
-            proto1 = sum(vals1(1:5))/5;
         end
+        [vals1] = sort(Class1min);
+        proto1 = sum(vals1(1:5))/5;
         for k = 1:length(X2)
             Class2min(k) = sqrt((X2(k,1) - XGrid(i,1))^2 + (X2(k,2) - XGrid(i,2))^2);
-            [vals2] = sort(Class2min);
-            proto2 = sum(vals2(1:5))/5;
         end
+        [vals2] = sort(Class2min);
+        proto2 = sum(vals2(1:5))/5;
+        
         if (proto1 < proto2)
             class(i) = 1;
         elseif (proto2 < proto1)
@@ -29,6 +30,6 @@ function kNN(X1,X2)
             class(i) = 0;
         end
     end
-    z = reshape(class,length(Y),length(X));
+    z = reshape(class,length(x2range),length(x1range));
     contour(xx1,xx2,z)
 end
